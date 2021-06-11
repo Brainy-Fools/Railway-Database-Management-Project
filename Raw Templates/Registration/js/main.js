c73e98614e7edf25aa3cfdff2b88b48637b4c02e
Abdullah Al Masum <201914044@student.mist.ac.bd>
2021-06-11 15:59:13 +0600

    Spent 3 whole Days to select + create proper front-end pages

---
+++ Raw Templates/Registration/js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+    $("#form-total").steps({
+        headerTag: "h2",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        stepsOrientation: "vertical",
+        autoFocus: true,
+        transitionEffectSpeed: 500,
+        titleTemplate : '<div class="title">#title#</div>',
+        labels: {
+            previous : 'Back Step',
+            next : '<i class="zmdi zmdi-arrow-right"></i>',
+            finish : '<i class="zmdi zmdi-check"></i>',
+            current : ''
+        },
+    })
+});
